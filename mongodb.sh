f37092c428c5b7cafa2a8ac44b55b11b21063d4a
heman chandra <hemanchandra701@gmail.com>
2026-07-11 16:10:57 +0530

    shell

---
+++ mongodb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m" 
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+mkdir -p $LOGS_FOLDER
+SCRIPIT_NAME=$(echo $0 | cut -d "." -f1)
+LOGS_FILE="$LOGS_FOLDER/$SCRIPIT_NAME.log"
+
+echo "Script started executed at : $(date)" | tee -a $LOGS_FILE
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ];then
+    echo -e "$R Please run this script with Root Access! $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];then
+        echo -e "$R $2 ... $N Failure" | tee -a $LOGS_FILE
+        exit 1
+    else 
+        echo -e "$G $2 ... $N Success" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGS_FILE
+VALIDATE $? "Setup the MongoDB repo file"
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Install MongoDB"
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod &>>$LOGS_FILE
+VALIDATE $? "Start MongoDB"
