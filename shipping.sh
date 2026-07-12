171eff9525c8ab38728538a1fc682ed1666dee65
heman chandra <hemanchandra701@gmail.com>
2026-07-12 16:59:22 +0530

    shell

--- shipping.sh
+++ shipping.sh
@@ -82,16 +82,16 @@ dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "install mysql client"
 
 
-INDEX=$(mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "USE cities;")
+mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 -e "USE cities;" &>>"$LOGS_FILE"
 
-if [ $INDEX -ne 1 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>"$LOGS_FILE"
+if [ $? -ne 0 ]; then
+    mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 < /app/db/schema.sql &>>"$LOGS_FILE"
     VALIDATE $? "Load schema to database"
 
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>"$LOGS_FILE"
+    mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 < /app/db/app-user.sql &>>"$LOGS_FILE"
     VALIDATE $? "Create application user"
 
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>"$LOGS_FILE"
+    mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 < /app/db/master-data.sql &>>"$LOGS_FILE"
     VALIDATE $? "Load master data"
 else
     echo -e "$Y Already loaded schema to the Database ...$N Skipping" | tee -a "$LOGS_FILE"
