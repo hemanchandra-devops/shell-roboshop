0bbc35c04259f6883e08beb59d47d26062379a22
heman chandra <hemanchandra701@gmail.com>
2026-07-12 16:51:11 +0530

    shell

--- shipping.sh
+++ shipping.sh
@@ -83,24 +83,19 @@ dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "install mysql client"
 
 
-mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 -e "USE cities;" &>>"$LOGS_FILE"
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "USE cities;" &>>$LOGS_FILE
 
 if [ $? -ne 0 ]; then
-    echo "Database 'cities' not found. Creating..."
-
-    mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 -e "CREATE DATABASE IF NOT EXISTS cities;" &>>"$LOGS_FILE"
-    VALIDATE $? "Create cities database"
-
-    mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 cities < /app/db/schema.sql &>>"$LOGS_FILE"
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>"$LOGS_FILE"
     VALIDATE $? "Load schema to database"
 
-    mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 cities < /app/db/app-user.sql &>>"$LOGS_FILE"
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>"$LOGS_FILE"
     VALIDATE $? "Create application user"
 
-    mysql -h "$MYSQL_HOST" -uroot -pRoboShop@1 cities < /app/db/master-data.sql &>>"$LOGS_FILE"
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>"$LOGS_FILE"
     VALIDATE $? "Load master data"
 else
-    echo -e "$Y Already loaded schema to the Database...$N Skipping" | tee -a "$LOGS_FILE"
+    echo -e "$Y Already loaded schema to the Database ...$N Skipping" | tee -a "$LOGS_FILE"
 fi
 
 systemctl restart shipping &>>$LOGS_FILE
